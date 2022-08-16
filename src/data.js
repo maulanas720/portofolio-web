61fe417f49187a8fede1e7c64c074ebff5d68d33
Maulana Sidiq <maulanas720@gmail.com>
2022-08-16 10:37:15 +0700

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://wisdmlabs.com/site/wp-content/uploads/2018/05/brock.jpg",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://images.unsplash.com/photo-1512484776495-a09d92e87c3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTJ8MTcyOTI1OHx8ZW58MHx8fHw%3D&w=1000&q=80",
+    link: "https://pythonbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://wisdmlabs.com/site/wp-content/uploads/2018/05/brock.jpg",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "https://images.unsplash.com/photo-1512484776495-a09d92e87c3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTJ8MTcyOTI1OHx8ZW58MHx8fHw%3D&w=1000&q=80",
+    link: "https://pythonbootcamp.com",
+  },
+];
+export const testimonials = [
+  {
+    quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis",
+    image: "https://wisdmlabs.com/site/wp-content/uploads/2018/05/brock.jpg",
+    name: "julia burch",
+    company: "gunadarma",
+  },
+  {
+    quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis",
+    image: "https://wisdmlabs.com/site/wp-content/uploads/2018/05/brock.jpg",
+    name: "julia burch",
+    company: "gunadarma",
+  }
+];
+
+export const skills = ["Javascript", "HTML", "CSS", "ReactJS", "Springboot", "Tailwind CSS"];
